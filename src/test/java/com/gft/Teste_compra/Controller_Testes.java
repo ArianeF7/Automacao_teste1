2a3bfdd2d24a1969346918472d93eb10dfbfa168
ArianeF7 <arianef.lima@yahoo.com.br>
2020-04-30 16:34:12 -0300

    Atualizações de boas práticas no código

--- src/test/java/com/gft/Teste_compra/Controller_Testes.java
+++ src/test/java/com/gft/Teste_compra/Controller_Testes.java
@@ -1,123 +1,132 @@
 package com.gft.Teste_compra;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.UnexpectedTagNameException;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
-
 import static org.junit.Assert.assertEquals;
 
 public class Controller_Testes {
+
+    ChromeDriver driver;
+    WebDriverWait wait;
+
+    @Before
+    public void init(){
+        System.setProperty("webdriver.chrome.driver", "src\\test\\Resources\\chromedriver.exe");
+
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 30);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown (){
+        driver.close();
+    }
+
     @Test
     public void testRealizarCompra() throws InterruptedException {
 
-               System.setProperty("webdriver.chrome.driver", "src\\test\\Resources\\chromedriver.exe");
+        driver.get("http://automationpractice.com/index.php");
 
-                WebDriver driver = new ChromeDriver();
-                driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        String produto = "blouse";
+        WebElement caixaBusca = driver.findElement(By.id("search_query_top"));
+        caixaBusca.sendKeys(produto);
 
-                driver.get("http://automationpractice.com/index.php");
+        driver.findElement(By.name("submit_search")).click();
 
-                String produto = "blouse";
-                WebElement caixaBusca = driver.findElement(By.id("search_query_top"));
-                caixaBusca.sendKeys(produto);
+        Thread.sleep(3000);
+        driver.findElement(By.className("ajax_add_to_cart_button")).click();
 
-                driver.findElement(By.name("submit_search")).click();
+        WebElement btnTesteRubens = wait.until(ExpectedConditions.elementToBeClickable(By.className("button-medium")));
+        btnTesteRubens.click();
 
-                driver.findElement(By.className("ajax_add_to_cart_button")).click();
+        String confProd = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#order-detail-content .product-name a"))).getText().toLowerCase();
+        assertEquals(produto,confProd);
 
-                WebDriverWait wait = new WebDriverWait(driver, 10);
+        String valTotal = driver.findElement(By.id("total_price")).getText();
 
-                driver.findElement(By.className("button-medium")).click();
+        driver.findElement(By.className("standard-checkout")).click();
 
-                Thread.sleep(1000);
-                String confereProduto = driver.findElement(By.cssSelector("#order-detail-content .product-name a")).getText().toLowerCase();
-                assertEquals(produto,confereProduto);
+        WebElement inserirEmail = driver.findElement(By.id("email_create"));
+        inserirEmail.sendKeys("testeAriane26@email.com");
 
-                String valTotal = driver.findElement(By.id("total_price")).getText();
+        driver.findElement(By.id("SubmitCreate")).click();
 
-                Thread.sleep(3000);
-                driver.findElement(By.className("standard-checkout")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("id_gender2"))).click();
 
-                WebElement inserirEmail = driver.findElement(By.id("email_create"));
-                inserirEmail.sendKeys("testeAriane16@email.com");
+        WebElement primeiroNome = driver.findElement(By.id("customer_firstname"));
+        primeiroNome.sendKeys("Ariane");
 
-                driver.findElement(By.id("SubmitCreate")).click();
+        WebElement ultimoNome = driver.findElement(By.id("customer_lastname"));
+        ultimoNome.sendKeys("Lima");
 
-                Thread.sleep(2000);
-                driver.findElement(By.id("id_gender2")).click();
+        WebElement senhaNewUser = driver.findElement(By.id("passwd"));
+        senhaNewUser.sendKeys("teste");
 
-                WebElement primeiroNome = driver.findElement(By.id("customer_firstname"));
-                primeiroNome.sendKeys("Ariane");
+        WebElement diaNasc = driver.findElement(By.id("days"));
+        diaNasc.sendKeys("7");
 
-                WebElement ultimoNome = driver.findElement(By.id("customer_lastname"));
-                ultimoNome.sendKeys("Lima");
+        WebElement mesNasc = driver.findElement(By.id("months"));
+        mesNasc.sendKeys("August");
 
-                WebElement senhaNewUser = driver.findElement(By.id("passwd"));
-                senhaNewUser.sendKeys("teste");
+        WebElement anoNasc = driver.findElement(By.id("years"));
+        anoNasc.sendKeys("1995");
 
-                WebElement diaNasc = driver.findElement(By.id("days"));
-                diaNasc.sendKeys("7");
+        WebElement Endereco1 = driver.findElement(By.id("address1"));
+        Endereco1.sendKeys("Alameda Rio Negro");
 
-                WebElement mesNasc = driver.findElement(By.id("months"));
-                mesNasc.sendKeys("August");
+        WebElement Endereco2 = driver.findElement(By.id("address2"));
+        Endereco2.sendKeys("Prédio");
 
-                WebElement anoNasc = driver.findElement(By.id("years"));
-                anoNasc.sendKeys("1995");
+        WebElement cidade = driver.findElement(By.id("city"));
+        cidade.sendKeys("Barueri");
 
-                WebElement Endereco1 = driver.findElement(By.id("address1"));
-                Endereco1.sendKeys("Alameda Rio Negro");
+        WebElement estado = driver.findElement(By.id("id_state"));
+        estado.sendKeys("Alaska");
 
-                WebElement Endereco2 = driver.findElement(By.id("address2"));
-                Endereco2.sendKeys("Prédio");
+        WebElement cep = driver.findElement(By.id("postcode"));
+        cep.sendKeys("00000");
 
-                WebElement cidade = driver.findElement(By.id("city"));
-                cidade.sendKeys("Barueri");
+        WebElement pais = driver.findElement(By.id("id_country"));
+        pais.sendKeys("United States");
 
-                WebElement estado = driver.findElement(By.id("id_state"));
-                estado.sendKeys("Alaska");
+        WebElement phone = driver.findElement(By.id("phone_mobile"));
+        phone.sendKeys("5515981206345");
 
-                WebElement cep = driver.findElement(By.id("postcode"));
-                cep.sendKeys("00000");
+        WebElement endOutro = driver.findElement(By.id("alias"));
+        endOutro.clear();
+        endOutro.sendKeys("Rua das rosas azuis");
 
-                WebElement pais = driver.findElement(By.id("id_country"));
-                pais.sendKeys("United States");
+        driver.findElement(By.id("submitAccount")).click();
 
-                WebElement phone = driver.findElement(By.id("phone_mobile"));
-                phone.sendKeys("5515981206345");
+        String validaEndereco = driver.findElement(By.cssSelector("#uniform-id_address_delivery.selector span")).getText().toLowerCase();
+        assertEquals("rua das rosas azuis",validaEndereco);
 
-                WebElement endOutro = driver.findElement(By.id("alias"));
-                endOutro.clear();
-                endOutro.sendKeys("Rua das rosas azuis");
+        driver.findElement(By.name("processAddress")).click();
 
-                driver.findElement(By.id("submitAccount")).click();
+        driver.findElement(By.id("cgv")).click();
 
-                String validaEndereco = driver.findElement(By.cssSelector("#uniform-id_address_delivery.selector span")).getText().toLowerCase();
-                assertEquals("rua das rosas azuis",validaEndereco);
+        driver.findElement(By.name("processCarrier")).click();
 
-                driver.findElement(By.name("processAddress")).click();
+        String validaTotal = driver.findElement(By.id("total_price")).getText();
+        assertEquals(valTotal, validaTotal);
 
-                driver.findElement(By.id("cgv")).click();
+        driver.findElement(By.className("bankwire")).click();
 
-                driver.findElement(By.name("processCarrier")).click();
+        driver.findElement(By.xpath("//*[@id=\"cart_navigation\"]/button")).submit();
 
-                String validaTotal = driver.findElement(By.id("total_price")).getText();
-                assertEquals(valTotal, validaTotal);
-
-                driver.findElement(By.className("bankwire")).click();
-
-                WebElement finalizaCompra = driver.findElement(By.xpath("//*[@id=\"cart_navigation\"]/button"));
-                finalizaCompra.submit();
-
-                String validaCompra = driver.findElement(By.className("page-heading")).getText();
-                assertEquals("ORDER CONFIRMATION", validaCompra);
-
-                Thread.sleep(3000);
-                driver.close();
-            }
+        String validaCompra = driver.findElement(By.className("page-heading")).getText();
+        assertEquals("ORDER CONFIRMATION", validaCompra);
     }
+}
 
